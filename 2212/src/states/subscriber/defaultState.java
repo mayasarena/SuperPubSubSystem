522dc84afd66c557d8e1bacf67b4f3eaf690f72f
Fletcher Smith <fsmith26@uwo.ca>
2019-04-07 13:06:28 -0400

    event factory and defaults

---
+++ 2212/src/states/subscriber/defaultState.java
@@ -0,0 +1,13 @@
+package states.subscriber;
+
+import events.AbstractEvent;
+
+public class defaultState implements IState {
+
+	@Override
+	public void handleEvent(AbstractEvent event, String channelName) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
